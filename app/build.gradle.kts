ec63a8ba9bdaee5ace6b48474277a90f288ef897
Davide Rosa <rsodvd79@gmail.com>
2025-05-31 17:42:38 +0200

    Bump version code and version name to 20250601 and 2025.06.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "it.drhack.smstomail"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20250531
-        versionName = "2025.05.31"
+        versionCode = 20250601
+        versionName = "2025.06.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
